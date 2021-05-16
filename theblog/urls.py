832d3c4771f4ec94326937ac765b1ceeb90b30b9
Katerina Odabasi <lyrisha@gmail.com>
2021-05-16 19:12:05 +0200

    Unify post urls, add contact links

--- theblog/urls.py
+++ theblog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('posts/<slug:slug>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name="add-post"),
     path('add_category/', AddCategoryView.as_view(), name="add-category"),
-    path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update-post"),
-    path('article/<int:pk>/delete', DeletePostView.as_view(), name="delete-post"),
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name="update-post"),
+    path('post/<int:pk>/delete', DeletePostView.as_view(), name="delete-post"),
     path('category/<str:cats>', CategoryView, name="category"),
 
 ]
